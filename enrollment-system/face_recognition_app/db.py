840543de48d2dce0990b7743b1e35d4f38aee681
chinocchio <chinonoble07@gmail.com>
2025-10-02 15:04:19 +0000

    Add server-side changes and face recognition updates

--- enrollment-system/face_recognition_app/db.py
+++ enrollment-system/face_recognition_app/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",      # change if remote
-        user="root",           # your MySQL username
-        password="",           # your MySQL password
+        user="sis_user",           # your MySQL username
+        password="P@$$word_Ch1no",           # your MySQL password
         database="enrollment_db"   # your Laravel DB name
     )
